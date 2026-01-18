ca6e3e3e9dbc40383ec209c07a6510b8be2b25b9
sagar <sagar.y.praveen@gmail.com>
2026-01-18 09:00:00 +0000

    Completed DSA challenge #18

---
+++ daily_practice_18.js
@@ -0,0 +1,19 @@
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
+  let res = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let l = i + 1, r = nums.length - 1;
+    while (l < r) {
+      let sum = nums[i] + nums[l] + nums[r];
+      if (sum === 0) {
+        res.push([nums[i], nums[l], nums[r]]);
+        while (l < r && nums[l] === nums[l + 1]) l++;
+        while (l < r && nums[r] === nums[r - 1]) r--;
+        l++; r--;
+      } else if (sum < 0) l++;
+      else r--;
+    }
+  }
+  return res;
+}
