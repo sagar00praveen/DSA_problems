a52dc9cf478c4182f2dff8673b3525cb67a8117c
sagar <sagar.y.praveen@gmail.com>
2026-01-19 09:00:00 +0000

    Completed DSA challenge #19

---
+++ daily_practice_19.js
@@ -0,0 +1,9 @@
+function maxArea(height) {
+  let l = 0, r = height.length - 1, max = 0;
+  while (l < r) {
+    max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
+    if (height[l] < height[r]) l++;
+    else r--;
+  }
+  return max;
+}
