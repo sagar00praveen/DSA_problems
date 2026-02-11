ddee8c3d306279caf6518bd5dba782db4fefcd89
sagar <sagar.y.praveen@gmail.com>
2026-02-11 10:52:32 +0530

    Add constant time complexity example code

---
+++ Arrays/basics.js
@@ -0,0 +1,26 @@
+let arr =[1,3,5,6,7,8];
+console.log(arr[0]);// index 0
+console.log(arr[1]);// index 1
+console.log(arr[2]); //index 2
+console.log(arr[3]); //index 3
+console.log(arr[4]); //index 4
+console.log(arr[5]); //index 5
+console.log(arr[6]); //index 6
+console.log(arr[7]); //index 7
+console.log(arr[8]); //index 8
+
+/*
+output:
+3
+5
+6
+7
+8
+undefined
+undefined
+undefined*/
+ 
+
+for (i=0;i<6;i++){
+   console.log(arr[i]);
+}
