824192108d7014ba8556fa14f184abb5f65b59ce
sagar <sagar.y.praveen@gmail.com>
2026-02-17 09:00:00 +0000

    Completed DSA challenge #48

---
+++ daily_practice_48.js
@@ -0,0 +1,13 @@
+function combinationSum(candidates, target) {
+  let res = [];
+  function dfs(i, cur, total) {
+    if (total === target) { res.push([...cur]); return; }
+    if (i >= candidates.length || total > target) return;
+    cur.push(candidates[i]);
+    dfs(i, cur, total + candidates[i]);
+    cur.pop();
+    dfs(i + 1, cur, total);
+  }
+  dfs(0, [], 0);
+  return res;
+}
