cae5788a104e4bdc4a60daceb36d59b6e193f978
sagar <sagar.y.praveen@gmail.com>
2026-02-22 09:00:00 +0000

    Completed DSA challenge #53

---
+++ daily_practice_53.js
@@ -0,0 +1,10 @@
+function maxAreaOfIsland(grid) {
+  let max = 0;
+  function dfs(i, j) {
+    if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]===0) return 0;
+    grid[i][j] = 0;
+    return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1);
+  }
+  for(let i=0; i<grid.length; i++) for(let j=0; j<grid[0].length; j++) max = Math.max(max, dfs(i, j));
+  return max;
+}
