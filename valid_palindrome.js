b9ec646abfa6b7d330ed4eed7cce8877d528c7c7
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2026-03-03 22:57:28 +0530

    Update valid_palindrome.js

--- valid_palindrome.js
+++ valid_palindrome.js
@@ -1,26 +1,3 @@
-/*
-  CHALLENGE: Valid Palindrome
-  
-  A palindrome is a string that reads the same forward and backward.
-  Example: "madam" -> true
-  Example: "racecar" -> true
-  Example: "hello" -> false
-  
-  Your Task:
-  Write a function `isPalindrome(str)` that returns true if valid, false if not.
-  
-   Constraints:
-  - Use the TWO POINTERS technique.
-  - O(n) Time Complexity.
-  - O(1) Space Complexity (No creating new strings).
-
-  Hint:
-  - Pointer 1 at start (0).
-  - Pointer 2 at end (length - 1).
-  - Loop while left < right.
-  - If characters don't match -> return false immediately.
-  - If match, move pointers closer -> left++, right--.
-*/
 
 function isPalindrome(str) {
     // TODO: Write your code here
@@ -33,3 +10,8 @@ console.log(isPalindrome("madam"));   // true
 console.log(isPalindrome("racecar")); // true
 console.log(isPalindrome("hello"));   // false
 console.log(isPalindrome("abba"));    // true
+//method 2
+
+function isPalindrome(str) {
+    return str === str.split("").reverse().join("");
+}
