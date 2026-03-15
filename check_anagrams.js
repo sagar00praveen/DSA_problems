c05c8125e819fae448b08c96cc352ef6a2f0434e
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2026-03-15 16:13:26 +0530

    Implement anagram checking function

---
+++ check_anagrams.js
@@ -0,0 +1,9 @@
+function isAnagram(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    return str1.split("").sort().join("") === str2.split("").sort().join("");
+}
+
+console.log(isAnagram("listen", "silent"));
