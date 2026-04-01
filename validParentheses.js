197e084248b04bcc512556f525120e32d9e48aef
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2026-04-01 23:36:44 +0530

    Implement function to validate parentheses

---
+++ validParentheses.js
@@ -0,0 +1,26 @@
+function isValid(s) {
+    let stack = [];
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else {
+            if (stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+console.log(isValid("()"));
+console.log(isValid("()[]{}"));
+console.log(isValid("(]"));
+console.log(isValid("([)]"));
+console.log(isValid("{[]}"));
