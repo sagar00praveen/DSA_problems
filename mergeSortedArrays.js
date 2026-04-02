9bbdd15cbef6af105c24618604828aa1e2c5ab5d
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2026-04-02 22:58:35 +0530

    Create mergeSortedArrays.js

---
+++ mergeSortedArrays.js
@@ -0,0 +1,30 @@
+function mergeSortedArrays(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+console.log(mergeSortedArrays([1,3,5], [2,4,6]));
+console.log(mergeSortedArrays([1,2,3], [4,5,6]));
