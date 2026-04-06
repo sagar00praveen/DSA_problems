d21d8518ee9e7befed80bc7ac5073fa86f17a378
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2026-04-06 23:29:41 +0530

    Add maxArea function for water container problem
    Implement maxArea function to calculate maximum water container area.

---
+++ containerWithMostWater.js
@@ -0,0 +1,24 @@
+function maxArea(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+
+    while (left < right) {
+        let h = Math.min(height[left], height[right]);
+        let w = right - left;
+        let area = h * w;
+
+        max = Math.max(max, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max;
+}
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+console.log(maxArea([1,1]));
